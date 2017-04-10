ff911608705ab3d12d1851bb142c4b1c377cc7e6
roiik16 <roik1234@gmail.com>
2017-04-10 16:07:02 +0200

    OntriggerBill, character idle animation and background

---
+++ Assets/GUIScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIScript : MonoBehaviour
+{
+    public GameObject ImageObject;
+
+    void OnTriggerEnter2D (Collider2D c)
+    {
+        if (c.tag == "Bill")
+        {
+            ImageObject.SetActive(true);
+            Debug.Log("TRIGGERED");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D c)
+    {
+        ImageObject.SetActive(false);
+    }
+}
