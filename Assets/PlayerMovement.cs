3f1f133e2ebe70aa71cfda440d386b4c06f3193d
roiik16 <roik1234@gmail.com>
2017-04-10 12:02:33 +0200

    Player Movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    float speed = 5f;
+    Rigidbody2D playerRB;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        Move(h);
+    }
+
+    void Move(float h)
+    {
+        //refer to the current physics
+        Vector2 movement = playerRB.velocity;
+        movement.x = h * speed;
+
+        // make the character move 
+        playerRB.velocity = movement;
+    }
+}
