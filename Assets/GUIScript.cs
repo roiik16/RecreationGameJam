288876ba3d76920af77a0bd4d3fbe4d2671f6730
roiik16 <roik1234@gmail.com>
2017-04-11 11:58:31 +0200

    ANIM WORKING ALLZ
    All animation is working. Next up is musuem scene.

--- Assets/GUIScript.cs
+++ Assets/GUIScript.cs
@@ -6,18 +6,36 @@ using UnityEngine.UI;
 public class GUIScript : MonoBehaviour
 {
     public GameObject ImageObject;
+    public GameObject TVanim;
+
+    int tv = 0;
 
     void OnTriggerEnter2D (Collider2D c)
     {
         if (c.tag == "Bill")
         {
             ImageObject.SetActive(true);
-            Debug.Log("TRIGGERED");
+            Debug.Log("Bill Triggered");
         }
+
+        if (c.tag == "tv" && tv == 0)
+        {
+            TVanim.SetActive(true);
+            Debug.Log("TVAnim Triggered");
+            tv = 1;
+        }
+
+        else
+        {
+            TVanim.SetActive(false);
+        }
+
+       
     }
 
     void OnTriggerExit2D(Collider2D c)
     {
         ImageObject.SetActive(false);
+        TVanim.SetActive(false);
     }
 }
---
+++ Assets/gamemanager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamemanager : MonoBehaviour {
+    public static float MaximumX;
+    public static float MaximumY;
+    
+    void Start()
+    {
+        MaximumX = Camera.main.ViewportToWorldPoint(Vector3.one).x;
+        MaximumY = Camera.main.ViewportToWorldPoint(Vector3.one).y;
+    }
+}
